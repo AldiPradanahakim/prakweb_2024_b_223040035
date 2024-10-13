e37eb724bdce1cedc808deec125fc361b577da16
AldiPradanahakim <aldi.pradan.hakim26@gmail.com>
2024-10-13 09:08:35 +0700

    upload oop video 13 - abstract class

---
+++ PraktikumWeb/OOP/Video_13/abstract-class.php
@@ -0,0 +1,42 @@
+<?php
+
+
+abstract class Buah
+{
+    private $warna;
+
+    public function makan()
+    {
+        // kunyah
+        //nyam..nyam..nyam
+    }
+
+    public function setWarna($warna)
+    {
+        $this->warna = $warna;
+    }
+}
+
+// class Apel extends Buah
+// {
+//     public function makan()
+//     {
+//         // kunyah
+//         // sampai bagian tengah
+//     }
+// }
+
+// class Jeruk extends Buah
+// {
+//     public function makan()
+//     {
+//         // kupas
+//         // kunyah
+//     }
+// }
+
+// $apel = new Apel();
+// $apel->makan();
+
+// $buah = new Buah();
+// $buah->makan();
